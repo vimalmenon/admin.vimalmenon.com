4d53de310616fb4c3518e60cfe8e47963b96ac12
Vimal Menon <69777626+vimalsmenon@users.noreply.github.com>
2022-06-11 19:18:53 +0800

    Added HTML-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,33 @@
-const path = require('path');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: './src/index.tsx',
-  devtool: 'inline-source-map',
+  entry: "./src/index.tsx",
+  devtool: "inline-source-map",
   mode: "development",
   module: {
     rules: [
       {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
+        test: /\.ts(x?)$/,
+        use: "ts-loader",
         exclude: /node_modules/,
       },
     ],
   },
   resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
+    extensions: [".tsx", ".ts", ".js"],
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Application Title",
+      hash: false,
+      filename: "./index.html",
+      template: "./index.html",
+      inject: true,
+    }),
+  ],
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
   },
 };
