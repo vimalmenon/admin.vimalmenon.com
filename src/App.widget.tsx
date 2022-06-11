617c7c5975cffc2adc7974697e15861b6c9bb031
Vimal Menon <69777626+vimalsmenon@users.noreply.github.com>
2022-06-11 19:57:17 +0800

    Working on material UI

---
+++ src/App.widget.tsx
@@ -0,0 +1,15 @@
+import * as React from "react";
+import CssBaseline from '@mui/material/CssBaseline';
+import { ThemeProvider } from '@mui/material/styles';
+import { theme } from "./theme";
+
+export const App: React.FC = () => {
+    return (
+        <ThemeProvider theme={theme}>
+            <CssBaseline />
+            <div>
+                This is App
+            </div>
+        </ThemeProvider>
+    )
+}
--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,14 @@ import * as React from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
+import { App } from "./App.widget";
+
 const root = createRoot(document.getElementById("root"));
 
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <div>This is Vimal Menon</div>
-    </BrowserRouter>
-  </React.StrictMode>
+    <React.StrictMode>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </React.StrictMode>
 );
