b23ead9b2e844dea56b806e8830b8bd5e4186b33
Vimal Menon <69777626+vimalsmenon@users.noreply.github.com>
2022-06-11 19:42:26 +0800

    Added react-router-dom

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,13 @@
 import * as React from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 
+const root = createRoot(document.getElementById("root"));
 
-const root = createRoot(document.getElementById("root") as HTMLElement);
-
-
-root.render(<div>This is vimal Menon</div>);
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <div>This is Vimal Menon</div>
+    </BrowserRouter>
+  </React.StrictMode>
+);
