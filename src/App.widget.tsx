4dc2daab6d8fa29ea48a07adab9b4235bba49535
Vimal Menon <69777626+vimalsmenon@users.noreply.github.com>
2022-06-11 22:50:57 +0800

    Working on AppHeader

--- src/App.widget.tsx
+++ src/App.widget.tsx
@@ -1,11 +1,19 @@
 import * as React from 'react';
 
 import { Box } from '@mui/material';
+import { ThemeProvider } from '@mui/material/styles';
+
+import { Layout } from './common/Layout';
+import { theme } from './theme';
 
 export const App: React.FC = () => {
   return (
-    <Box component="main" sx={{ width: '100%', flexGrow: 1, p: { xs: 2, sm: 3 } }}>
-      <div>This is App</div>
-    </Box>
+    <ThemeProvider theme={theme}>
+      <Layout>
+        <Box component="main" sx={{ width: '100%', flexGrow: 1, p: { xs: 2, sm: 3 } }}>
+          <div>This is App</div>
+        </Box>
+      </Layout>
+    </ThemeProvider>
   );
 };
--- src/index.tsx
+++ src/index.tsx
@@ -1,25 +1,18 @@
 import * as React from 'react';
 
-import CssBaseline from '@mui/material/CssBaseline';
-import { ThemeProvider } from '@mui/material/styles';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 
 import { App } from './App.widget';
 import { AppContext } from './context';
-import { theme } from './theme';
 
 const root = createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-
+    <AppContext>
       <BrowserRouter>
-        <AppContext>
-          <App />
-        </AppContext>
+        <App />
       </BrowserRouter>
-    </ThemeProvider>
+    </AppContext>
   </React.StrictMode>
 );
