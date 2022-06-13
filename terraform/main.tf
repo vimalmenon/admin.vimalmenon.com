77a0d1814606a7c480993a517a3e68c27ceb56b3
Vimal Menon <69777626+vimalsmenon@users.noreply.github.com>
2022-06-14 06:38:05 +0800

    making terraform work

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,5 +4,7 @@ terraform {
     key     = "admin.justbigstore.com/terraform.tfstate"
     region  = "us-east-1"
     encrypt = true
+    access_key = var.ACCESS_KEY
+    secret_key = var.SECRET_KEY
   }
 }
