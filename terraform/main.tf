fb966a8a09206c2a115acb820dffd4432e4fa7ad
Vimal Menon <69777626+vimalsmenon@users.noreply.github.com>
2022-06-14 06:55:36 +0800

    Fix terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,7 +4,5 @@ terraform {
     key     = "admin.justbigstore.com/terraform.tfstate"
     region  = "us-east-1"
     encrypt = true
-    access_key = var.ACCESS_KEY
-    secret_key = var.SECRET_KEY
   }
 }
