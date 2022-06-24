7c48a00a40e7134801165eafeb5bff3c92a787b7
Vimal Menon <69777626+vimalsmenon@users.noreply.github.com>
2022-06-25 06:35:48 +0800

    Package Updated

--- src/pages/Setting/index.tsx
+++ src/pages/Setting/index.tsx
@@ -5,7 +5,7 @@ import { Box } from '@mui/material';
 export const Setting: React.FC = () => {
   return (
     <Box component="main" sx={{ width: '100%', flexGrow: 1, p: { xs: 2, sm: 3 } }}>
-      <div>This is App</div>
+      <div>This is Setting Page</div>
     </Box>
   );
 };
