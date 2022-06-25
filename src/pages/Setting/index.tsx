04ede0e46c38af045f8586951cded25d94ee32be
Vimal Menon <69777626+vimalsmenon@users.noreply.github.com>
2022-06-25 09:45:05 +0800

    fixing the eslint errors

--- src/pages/Setting/index.tsx
+++ src/pages/Setting/index.tsx
@@ -4,7 +4,7 @@ import { Box } from '@mui/material';
 
 export const Setting: React.FC = () => {
   return (
-    <Box component="main" sx={{ width: '100%', flexGrow: 1, p: { xs: 1} }}>
+    <Box component="main" sx={{ width: '100%', flexGrow: 1, p: { xs: 1 } }}>
       <div>This is Setting Page</div>
     </Box>
   );
